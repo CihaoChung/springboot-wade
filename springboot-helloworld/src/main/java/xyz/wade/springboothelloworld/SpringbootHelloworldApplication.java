0187345032a689daef893a87e3cf025bfd5fe082
Wadewhy <513985092@qq.com>
2019-12-29 11:04:36 +0800

    Initial commit

---
+++ springboot-helloworld/src/main/java/xyz/wade/springboothelloworld/SpringbootHelloworldApplication.java
@@ -0,0 +1,13 @@
+package xyz.wade.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHelloworldApplication.class, args);
+    }
+
+}
